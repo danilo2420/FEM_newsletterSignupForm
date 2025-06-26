71c6ca40e05948fb0c911b3d1fc17b41266a5264
dani <danimoraga1997@gmail.com>
2025-06-26 19:00:10 +0200

    Container1: button responds

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
 console.log('hello');
+
+/* Variables */
+const form1 = document.getElementById('form1');
+console.log(form1);
+
+/* Functions */
+function main() {
+    configure_form1();
+}
+
+function configure_form1() {
+    function handle_submit(e) {
+        console.log('hello');
+        e.preventDefault();
+    }
+    form1.addEventListener('submit', handle_submit);
+}
+
+
+main();
